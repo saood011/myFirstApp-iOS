be66ec69f9232097975e485455e5baae67791ca7
Mohd Saood Akhtar <48763014+saood011@users.noreply.github.com>
2023-04-05 17:01:05 +0200

    Initial Commit

---
+++ myFirstApp/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  myFirstApp
+//
+//  Created by saood.akhtar on 05.04.23.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
